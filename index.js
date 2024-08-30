a86e51b3a35a1358afaff19da9922dc4d7b9d3c3
Mudassar <iammudassarnaveed1@gmail.com>
2024-08-30 23:30:24 +0500

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+console.log('Hello World');
+
+
+
+// what is a varriable
+// --> varriables are the named memory location
+
+// in programming when we talsk about memory we are talking about RAM
+// our program run on the RAM...
+
+// as our webpage is running all the varraibles are in our RAM(as we close the tab)
+// it will destroy.
+let name = 'John';
+console.log('name', name);
